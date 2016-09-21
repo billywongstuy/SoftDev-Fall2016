fe7d6cb34acb1534c4fb1533e381e3949e13aa01
bwong5@stuy.edu <bwong5@stuy.edu>
2016-09-21 12:26:44 -0400

    hopefulyl no merge colly no merge conflicts

--- flasky/app.py
+++
@@ -1,8 +0,0 @@
-def hello_world():
-    returbn "No hablo queso"
-
-def foo():
-    print "BLANK"
-
-if __name__ = "__main__":
-    foo()
--- flasky/fun.py
+++
@@ -1,9 +0,0 @@
-from flask install Flask
-app = Flask(__name__)
-
-def greet():
-    return "hello"
-
-
-if __name__ = "__main__":
-    greet()
